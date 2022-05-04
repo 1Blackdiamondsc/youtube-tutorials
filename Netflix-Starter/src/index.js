ee88b08dc4767df75d04eecf9e65400044b3e9dd
BlackDiamond SC Inc [BDSCI TOKEN] <73549208+1Blackdiamondsc@users.noreply.github.com>
2022-05-04 17:00:19 -0400

    Update index.js

--- Netflix-Starter/src/index.js
+++ Netflix-Starter/src/index.js
@@ -11,7 +11,7 @@ import { NotificationProvider } from 'web3uikit';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider appId="xxxxx" serverUrl="xxxxx">
+    <MoralisProvider appId="D9H77DqILLuqqemNBNm861Iz0WyxK4MOzWQ30v0R" serverUrl="https://9gjpferypzp4.usemoralis.com:2053/server">
       <NotificationProvider>
       <BrowserRouter>
         <App />
